fbae52372c91e8d7a1a84da587613b9a7b0f64fc
unknown <samuel.x.speck@AFS72S5HRDAAA4.us.accenturefederal.com>
2015-03-20 15:48:36 -0500

    initial commit

---
+++ www/owner/login.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Restaurant Owner Login</title>
+</head>
+
+<body>
+	<form action="" method="post">
+	Username: <input type="text" name="username"><br>
+	Password: <input type="password" name="password"><br>
+	</form>
+</body>
+</html>
