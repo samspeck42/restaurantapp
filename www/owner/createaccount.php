b1f5123b8ee008926e6906e7fd93997cbbeb65ba
unknown <samspeck42@gmail.com>
2015-03-21 21:05:54 -0500

    create account form input validation

--- www/owner/createaccount.php
+++ www/owner/createaccount.php
@@ -1,3 +1,40 @@
+<?php
+	$firstName = $lastName = $username = $password = "";
+	$firstNameErr = $lastNameErr = $usernameErr = $passwordErr = "";
+	
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		if (empty($_POST["firstName"])) {
+			$firstNameErr = "*First name is required";
+		} else {
+			$firstName = test_input($_POST["firstName"]);
+		}
+		
+		if (empty($_POST["lastName"])) {
+			$lastNameErr = "*Last name is required";
+		} else {
+			$lastName = test_input($_POST["lastName"]);
+		}
+		
+		if (empty($_POST["username"])) {
+			$usernameErr = "*Username is required";
+		} else {
+			$username = test_input($_POST["username"]);
+		}
+		
+		if (empty($_POST["password"])) {
+			$passwordErr = "*Password is required";
+		} else {
+			$password = test_input($_POST["password"]);
+		}
+	}
+	
+	function test_input($inp) {
+		$inp = trim($inp);
+		$inp = stripslashes($inp);
+		$inp = htmlspecialchars($inp);
+		return $inp;
+	}
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,23 +44,23 @@
 <body>
 	<h2>Create Account:</h2>
 
-	<form action="login.php" method="post" onsubmit="return checkPassword()" name="accountForm">
+	<form action="createaccount.php" method="post" onsubmit="return checkPassword()" name="accountForm">
 		<table style="width:30%">
 			<tr>
 				<td>First Name: </td>
-				<td><input type="text" name="firstName"></td>
+				<td><input type="text" name="firstName" value="<?php echo $firstName; ?>"></td>
 			</tr>
 			<tr>
 				<td>Last Name: </td>
-				<td><input type="text" name="lastName"></td>
+				<td><input type="text" name="lastName" value="<?php echo $lastName; ?>"></td>
 			</tr>
 			<tr>
 				<td>Username: </td>
-				<td><input type="text" name="username"></td>
+				<td><input type="text" name="username" value="<?php echo $username; ?>"></td>
 			</tr>
 			<tr>
 				<td>Password: </td>
-				<td><input type="password" name="password"></td>
+				<td><input type="password" name="password" value="<?php echo $password; ?>"></td>
 			</tr>
 			<tr>
 				<td>Confirm Password: </td>
@@ -31,7 +68,23 @@
 			</tr>
 		</table>
 		
-		<span style="color:#FF0000" id="passwordError"></span>
+		<br>
+		<span style="color:#FF0000" id="error">
+		<?php
+		if (!empty($firstNameErr)) {
+			echo $firstNameErr . "<br>";
+		}
+		if (!empty($lastNameErr)) {
+			echo $lastNameErr . "<br>";
+		}
+		if (!empty($usernameErr)) {
+			echo $usernameErr . "<br>";
+		}
+		if (!empty($passwordErr)) {
+			echo $passwordErr . "<br>";
+		}
+		?>
+		</span>
 		<br>
 		<input type="submit" value="Submit">
 	</form>
@@ -41,10 +94,10 @@
 		var pwrd = document.forms["accountForm"]["password"].value;
 		var confirmPwrd = document.forms["accountForm"]["confirmPassword"].value;
 		if (pwrd !== confirmPwrd) {
-			document.getElementById("passwordError").innerHTML="Passwords don't match";
+			document.getElementById("error").innerHTML="*Passwords don't match<br>";
 			return false;
 		} else {
-			document.getElementById("passwordError").innerHTML.value="";
+			document.getElementById("error").innerHTML.value="";
 			return true;
 		}
 	}
